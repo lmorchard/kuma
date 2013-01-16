5df3312f660cac3ddf1ab10025eab566f3e5ce62
Les Orchard <me@lmorchard.com>
2013-01-16 16:30:32 -0600

    bug 831444: Remove customercare app (part 2)

--- settings.py
+++ settings.py
@@ -826,20 +826,6 @@ THUMBNAIL_PROGRESS_WIDTH = 32  # width of the above image
 THUMBNAIL_PROGRESS_HEIGHT = 32  # height of the above image
 VIDEO_MAX_FILESIZE = 16777216  # 16 megabytes, in bytes
 
-# Customer Care settings
-CC_MAX_TWEETS = 500   # Max. no. of tweets in DB
-CC_TWEETS_PERPAGE = 100   # How many tweets to collect in one go. Max: 100.
-CC_SHOW_REPLIES = True  # Show replies to tweets?
-
-CC_TWEET_ACTIVITY_URL = 'https://metrics.mozilla.com/stats/twitter/armyOfAwesomeKillRate.json'  # Tweet activity stats
-CC_TOP_CONTRIB_URL = 'https://metrics.mozilla.com/stats/twitter/armyOfAwesomeTopSoldiers.json'  # Top contributor stats
-CC_TWEET_ACTIVITY_CACHE_KEY = 'sumo-cc-tweet-stats'
-CC_TOP_CONTRIB_CACHE_KEY = 'sumo-cc-top-contrib-stats'
-CC_STATS_CACHE_TIMEOUT = 24 * 60 * 60  # 24 hours
-CC_STATS_WARNING = 30 * 60 * 60  # Warn if JSON data is older than 30 hours
-CC_IGNORE_USERS = ['fx4status']  # User names whose tweets to ignore.
-
-
 NOTIFICATIONS_FROM_ADDRESS = 'notifications@support.mozilla.com'
 # Anonymous watches must be confirmed.
 CONFIRM_ANONYMOUS_WATCHES = True
